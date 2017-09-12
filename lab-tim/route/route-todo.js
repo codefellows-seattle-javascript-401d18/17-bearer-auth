d5aa6cbd6c55a0a3dd4c48d3582046c09bb71fd9
Tim Turner <timmyturner@me.com>
2017-09-12 14:35:11 -0700

    adds bearerAuth stuffs

--- lab-tim/route/route-todo.js
+++ lab-tim/route/route-todo.js
@@ -26,15 +26,27 @@ module.exports = function(router) {
       .catch(err => errorHandler(err, req, res));
   });
 
-  router.get('/api/todo', (req, res) => {
+  router.get('/api/todo', bearerAuth, (req, res) => {
+    debug('GET /api/todo/');
 
+    return Todo.find()
+      .then(todo => res.json(todo.map(todo => todo._id)))
+      .catch(err => errorHandler(err, req, res));
   });
 
-  router.put('/api/todo', (req, res) => {
+  router.put('/api/todo/:_id', bearerAuth, (req, res) => {
+    debug('PUT /api/todo/:_id');
 
+    return Todo.findByIdAndUpdate(req.params._id, req.body, { upsert:true, runValidators:true })
+      .then(() => res.status(204))
+      .catch(err => errorHandler(err, req, res));
   });
 
-  router.delete('/api/todo', (req, res) => {
+  router.delete('/api/todo/:_id', bearerAuth, (req, res) => {
+    debug('DELETE /api/todo/:_id');
 
+    return Todo.findByIdAndRemove(req.params._id)
+      .then(() => res.sendStatus(204))
+      .catch(err => errorHandler(err, req, res));
   });
 };
